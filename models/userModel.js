fc61e2bb70eff1fbf3a400bdbe1d20cd05ce0e39
jtkyber <43868043+jtkyber@users.noreply.github.com>
2025-02-24 02:08:23 -0500

    Fix

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,33 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
 
 const photoSchema = new Schema({
-    key: String,
-    tags: [String],
-    description: String,
-    location: String
-})
+	key: String,
+	tags: [String],
+	description: String,
+	location: String,
+});
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    allTags: [{
-        name: String, 
-        weight: Number, 
-        count: Number
-    }],
-    photos : [photoSchema],
-    bgImage: String
-})
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	allTags: [
+		{
+			name: String,
+			weight: Number,
+			count: Number,
+		},
+	],
+	photos: [photoSchema],
+	bgImage: String,
+});
 
-const User = models.User29 || model('User29', userSchema, "users");
+const User = models.User30 || model('User30', userSchema, 'users');
 
 export default User;
